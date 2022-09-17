876417a942a8b363ac2de787fc9340c3ddc7b161
Mariah Sekyi <mariahsekyi@wustl.edu>
2022-09-17 12:31:03 -0500

    Completed studio
    Finished all of the tasks

--- scripts/ts/dynamic-dom/sticky.js
+++ scripts/ts/dynamic-dom/sticky.js
@@ -1,3 +1,17 @@
 export function setupSticky(){
-    
+    console.log("sticky javascript is working");
+    window.onscroll = function() {
+        stickHeader();
+    }
+    var header = document.getElementById("main-menu-container");
+    var sticky = header.offsetTop;
+
+    function stickHeader(){
+        if (window.pageYOffset > sticky){
+            header.classList.add("sticky");
+        }else{
+            header.classList.remove("sticky");
+        }
+
+    }
 }
